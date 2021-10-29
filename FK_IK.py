883ddbed857fb907de8b9b510a07d4c26d1e49d6
Nils Wilhelm Becker <nilbecke@mail.uni-mannheim.de>
2021-10-29 11:45:53 +0200

    Setting up repo and first methods on FK and IK

---
+++ FK_IK.py
@@ -0,0 +1,23 @@
+def FK_solve(q, flag):
+    '''
+    Solves forward kinematics for given angles.
+    :param q: List of input angles
+    :param flag: Indicates output
+    :returns: If flag = "ee": 4x4 homogenous transformation matrix from base frame to end effector frame 
+              If flag = "full": All transformation matrices from base to end effector frame
+    '''
+    pass
+
+def transform_base(trans):
+    '''
+    Places Robot arm somewhere based on input.
+    :param trans: Determines 
+    :returns: New Base of robot
+    '''
+    pass
+
+def IK_solve(base_frame, ee_frame):
+    '''
+    
+    '''
+    pass
